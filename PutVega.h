cb9ea1ee4d35ccd34e513d78d48702bdb49a603b
Amandeep Mander <aman.mander86@gmail.com>
2014-10-23 13:34:26 -0400

    Create PutVega.h

---
+++ PutVega.h
@@ -0,0 +1,16 @@
+//
+//  PutVega.h
+//  RatioProtectivePut
+//
+//  Created by Aman on 2014-10-23.
+//  Copyright (c) 2014 Aman. All rights reserved.
+//
+
+#ifndef __RatioProtectivePut__PutVega__
+#define __RatioProtectivePut__PutVega__
+
+#include "ProbGenerator.h"
+
+double put_vega (const double S, const double K, const double r, const double v, const double T);
+
+#endif /* defined(__RatioProtectivePut__PutVega__) */
